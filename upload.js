b27f6503e6fbccea08408117c9d7f19a6c0c478c
Isaac Olawale <bodolawale@gmail.com>
2021-05-22 12:29:00 +0100

    removed trycatch

--- upload.js
+++ upload.js
@@ -6,17 +6,13 @@ class ImageUpload {
 		return res.status(200).send({ message: "Image uploaded successfully" });
 	}
 	static downloadImage(req, res) {
-		try {
-			const stream = StorageService.downloadFile(req.params.key).on(
-				"error",
-				(err) => {
-					return res.status(err.statusCode).send({ message: err.message });
-				}
-			);
-			stream.pipe(res);
-		} catch (error) {
-			return res.status(404).send({ message: error.message });
-		}
+		const stream = StorageService.downloadFile(req.params.key).on(
+			"error",
+			(err) => {
+				return res.status(err.statusCode).send({ message: err.message });
+			}
+		);
+		stream.pipe(res);
 	}
 }
 
